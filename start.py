92fb8304e6fa027fed152bc08d4a410f2b32b7b7
skim <rumex13@naver.com>
2023-02-25 17:06:59 +0900

    update sensitivites

--- start.py
+++ start.py
@@ -2,6 +2,6 @@ import os
 import platform
 
 if platform.system() == "Darwin":
-    os.system("python3 wake_word_util.py --model_path resources/ko.pv --keyword_paths resources/vanilla_mac.ppn")
+    os.system("python3 wake_word_util.py --model_path resources/ko.pv --keyword_paths resources/vanilla_mac.ppn --sensitivities 0.8")
 else:
-    os.system("python3 wake_word_util.py --model_path resources/ko.pv --keyword_paths resources/vanilla.ppn")
+    os.system("python3 wake_word_util.py --model_path resources/ko.pv --keyword_paths resources/vanilla.ppn --sensitivities 0.8")
